35bca2fc1f2e795a1a6a309ae02ae60179b1a4e7
Felipe Lima <flf.felipe@gmail.com>
2019-09-29 21:05:33 -0300

    Updates .gitignore

--- src/main/java/com/knight/estoque/servicos/ListagemLivros.java
+++ src/main/java/com/knight/estoque/servicos/ListagemLivros.java
@@ -19,6 +19,6 @@ public class ListagemLivros {
 	public static void main(String[] args) {
 		Endpoint.publish("http://localhost:8080/livros", new ListagemLivros());
 		
-		System.out.println("Serviço inciado!");
+		System.out.println("Serviço iniciado!");
 	}
 }
